234fb59edc68334402b2f204b23686975862b9f6
Tran Trung Tinh <trantrungtinh4954@gmail.com>
2018-04-01 00:25:52 +0700

    init location

--- README.md
+++ README.md
@@ -1,23 +1,5 @@
 <snippet>
   <content>
-
-# BOOKING_A_CAR
-
-Mini project clone Uber app with iOS 11
- 
-Example project using Swift 4 with Firebase database
-
-## Installation
-```
-git clone https://github.com/TranTrungTinh/booking_a_car.git
-```
-
-## Usage
-```
-1. Open booking_a_car.xcworkspace
-2. Choose simulator
-3. Run
-```
-
+  DO AN CUOI KY MON CONG NGHE PHAN MEM
   </content>
 </snippet>
--- booking_a_car/Controller/HomeVC.swift
+++ booking_a_car/Controller/HomeVC.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 import RevealingSplashView
 
 class HomeVC: UIViewController, MKMapViewDelegate {
@@ -16,27 +17,63 @@ class HomeVC: UIViewController, MKMapViewDelegate {
     @IBOutlet weak var actionBtn: RoundedShadowButton!
     
     var delegate: CenterVCDelegate?
+    var maneger: CLLocationManager?
+    var regionRadius: CLLocationDistance = 1000
+    
     let revealingSplashView = RevealingSplashView(iconImage: #imageLiteral(resourceName: "launchScreenIcon"), iconInitialSize: CGSize(width: 80, height: 80), backgroundColor: #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1))
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        maneger = CLLocationManager()
+        maneger?.delegate = self
+        maneger?.requestWhenInUseAuthorization()
+        maneger?.desiredAccuracy = kCLLocationAccuracyBest
+        checkLocationAuthStatus()
+        
         mapView.delegate = self
+        
+        centerMapOnUserLocation()
         
         self.view.addSubview(revealingSplashView)
         revealingSplashView.animationType = SplashAnimationType.heartBeat
         revealingSplashView.startAnimation()
         revealingSplashView.heartAttack = true
     }
+    
+    func checkLocationAuthStatus() {
+        if CLLocationManager.authorizationStatus() == .authorizedAlways {
+            maneger?.startUpdatingLocation()
+        } else {
+            maneger?.requestAlwaysAuthorization()
+        }
+    }
 
+    func centerMapOnUserLocation() {
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(mapView.userLocation.coordinate, regionRadius * 2.0, regionRadius * 2.0)
+        mapView.setRegion(coordinateRegion, animated: true);
+    }
+    
     @IBAction func actionBtnWasPressed(_ sender: Any) {
         actionBtn.animateButton(shouldLoad: true, withMessage: nil)
+    }
+    
+    @IBAction func centerMapBtnWasPressed(_ sender: Any) {
+        centerMapOnUserLocation()
     }
     
     @IBAction func menuBtnWasPressed(_ sender: Any) {
         delegate?.toggleLeftPanel()
     }
     
-    
+}
 
+extension HomeVC: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedAlways {
+            mapView.showsUserLocation = true
+            mapView.userTrackingMode = .follow
+        }
+    }
 }
 
